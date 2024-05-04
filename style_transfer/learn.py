675aff351a48ae91b7e557bc1b2e0cd9a50aa19d
vhsok <168888594+vhsok@users.noreply.github.com>
2024-05-04 02:20:41 -0300

    Update learn.py

--- style_transfer/learn.py
+++ style_transfer/learn.py
@@ -5,50 +5,6 @@ from .loss import VGG19Loss
 from .amp import GradScaler
 
 class StyleTransfer(object):
-    """The central object that performs style transfer when called.
-    
-    :param lr: Learning rate of the optimizer.
-    :type lr: float
-    :param content_weight: Weight for content loss.
-    :type content_weight: int
-    :param style_weight: Weight for style loss.
-    :type style_weight: int
-    :param content_weights: Weights of content loss for each layer.
-    :type content_weights: str
-    :param style_weights: Weights of style loss for each layer.
-    :type style_weights: str
-    :param avg_pool: If ``True``, replaces max-pooling by average-pooling.
-    :type avg_pool: bool
-    :param feature_norm: If ``True``, divides each ``style_weight`` by the 
-        square of the number of feature maps in the corresponding layer.
-    :type feature_norm: bool
-    :param weights: Weights of the VGG19 Network. Either ``'original'`` 
-        weights or ``'normalized'`` weights that are scaled such that the 
-        mean activation of each convolutional filter over images
-        and positions is equal to one.
-    :type weights: str
-    :param preserve_color: If set to ``'style'``, changes color of content 
-        image to color of style image. If set to ``'content'``, changes 
-        color of style image to color of content image. If set to 
-        ``None``, does not change any color.
-    :type preserve_color: Union[str, None]
-    :param device: Set to ``'cpu'`` to use CPU, ``'cuda'`` to use GPU  or 
-        ``'auto'`` to automatically choose device.
-    :type device: str
-    :param use_amp: If ``True``, uses automatic mixed precision for 
-        training.
-    :type use_amp: bool
-    :param adam: If ``True``, uses Adam instead of LBFGS optimizer.
-    :type adam: bool
-    :param optim_cpu: If ``True``, optimizes artwork on CPU, but can 
-        calculate gradients on GPU. This moves some data from GPU memory 
-        to working memory, but increases training time.
-    :type optim_cpu: bool
-    :param logging: Number of iterations between logs. If set to ``0``, does 
-        not log.
-    :type logging: int
-    """
-
     def __init__(self, lr=1, content_weight=1, style_weight=1e3,
                  content_weights="{'relu_4_2':1}", style_weights=("{'relu_1_1':"
                  "1,'relu_2_1':1,'relu_3_1':1,'relu_4_1':1,'relu_5_1':1}"),
@@ -71,32 +27,7 @@ class StyleTransfer(object):
         self.logging = logging
 
     def __call__(self, content, style, area=512, init_random=False,
-                 init_img=None, iter=500):
-        """Returns artwork created from content and style image.
-
-        :param content: Content image.
-        :type content: PIL.Image.Image
-        :param style: Style image.
-        :type style: PIL.Image.Image
-        :param area: ``content`` and ``style`` are scaled such that their area 
-            is ``area`` * ``area``. ``artwork`` has the same shape as 
-            ``content``.
-        :type area: int
-        :param init_random: If ``True``, initializes ``artwork`` with random 
-            image.
-        :type init_random: bool
-        :param init_img: Image with which ``artwork`` is initialized. If set 
-            to ``None``, initializes ``artwork`` either with ``content`` or 
-            randomly.
-        :type init_img: Union[PIL.Image.Image, None]
-        :param iter: Number of iterations.
-        :type iter: int
-        :return: ``artwork`` that matches content of ``content`` and style of 
-            ``style``.
-        :rtype: PIL.Image.Image
-        """
-
-        assert not (init_random and init_img)
+                 init_img=None, iter=500, model_path='model.pth'):
         artwork, optimizer, scaler = self._init_call(content, style, area, 
                                                      init_random, init_img)
         i = 0
@@ -119,6 +50,8 @@ class StyleTransfer(object):
         if self.logging:
             logger.close()
         self.criterion.reset()
+        # Salvar o modelo após o treinamento
+        torch.save(self.criterion.state_dict(), model_path)
         return self.postprocess(artwork)
 
     def _init_call(self, content, style, area, init_random, init_img):
